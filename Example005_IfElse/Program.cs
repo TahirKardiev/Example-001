0e2172e2fa15b1b3d17a01ba5249c5d139059bbf
Andrei <mr.fesenko@inbox.ru>
2022-08-05 14:09:15 +0300

    add masha

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, this is MASHA");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
