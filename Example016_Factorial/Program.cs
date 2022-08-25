83ec5f37f4529b4aa08ecbf4b76cee8336530792
Andrew <mr.fesenko@inbox.ru>
2022-08-25 15:58:14 +0300

    added ex16

---
+++ Example016_Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿double Factorial (int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+Console.WriteLine(Factorial(5));
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
